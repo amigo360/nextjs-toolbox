e9d4ca1bf66814d695cfc45bf68253a155fe28af
amigo360 <113510952+amigo360@users.noreply.github.com>
2022-09-14 14:42:29 +0530

    changes
    dones

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,20 @@ export default function Home() {
         <Header title="Next.js Toolbox" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
+        Are you worried about your family and friends? Are you looking for the best Friends & <a href="https://play.google.com/store/apps/details?id=com.amigo360.family.circle.friends.tracker">family tracking app</a>? Here is the end of your search for the family & Friends locator GPS tracker app. Using this app to find family & friends, you can keep track of everyone you know.
+
+
+
+With the <a href="https://www.amigo360.app/">Amigo360</a> Family Locator app, you can use the GPS location of your family members' phones to find out where they are on a live map. This is an intelligent way to keep track of your family's location in real time. All you have to do is put the Amigo360 location tracking and family locator app on the phone you want to use, sign up with the phone number, add the people you want to track to your group, and then start monitoring them. This is the best app for finding family & Friends.
+
+
+You don't have to use different apps to keep track of your friends, family, employees, and vehicles. Using the "/<a href="https://apps.apple.com/us/app/amigo360/id1634143090">Family Locator app</a>," you can find out where your family is and if they are safe. You can see where all your friends are on a map by using Friends Location Tracking. In the same way, you can always keep track of your On-Ground Staff. Using a family locator app, you can also track a vehicle.
+
+
+When choosing an Amigo360 Upgrade plan, your family can access extra features like 24/7 Roadside Assistance, SOS alerts for when you're in a scary situation, and much more.
+
+
+You can use our Amigo360 app to track Friends & Family Members. This was the <a href="https://www.districtsinfo.com/2022/07/5-best-family-tracker-apps-for-android-ios.html">Best Friends Tracking App </a>in Android & IOS
         </p>
         <FeedbackForm />
         <JokeBlock />
